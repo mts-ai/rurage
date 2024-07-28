c0734b21c380e2be522f5fc58a870e82e2aa841e
KraykoN <krayko13@gmail.com>
2024-07-28 18:00:27 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="RuRAGE (Russian RAG Evaluation) is a Python library developed to speed-up evaluation russian RAG systems with Correctness, Faithfulness and Relevance axes using a variety of deterministic and model-based metrics.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Sidorov Ivan MTS AI, Kraiko Nikita MTS AI",
+    author="Sidorov Ivan MTS AI, Krayko Nikita MTS AI",
     author_email="i.sidorov1@mts.ai, n.kraiko@mts.ai",
     url="https://github.com/mts-ai/rurage",
     packages=find_packages(),
